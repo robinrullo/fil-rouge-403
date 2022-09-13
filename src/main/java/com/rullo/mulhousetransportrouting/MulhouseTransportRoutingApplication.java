0a528f71bc40c26eff2c831c458a7d9516156af0
r-rullo <r-rullo@logitud.fr>
2022-09-13 14:35:28 +0200

    chore: add checkstyle and test workflow

--- src/main/java/com/rullo/mulhousetransportrouting/MulhouseTransportRoutingApplication.java
+++ src/main/java/com/rullo/mulhousetransportrouting/MulhouseTransportRoutingApplication.java
@@ -3,6 +3,10 @@ package com.rullo.mulhousetransportrouting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Robin Rullo
+ */
+
 @SpringBootApplication
 public class MulhouseTransportRoutingApplication {
 
